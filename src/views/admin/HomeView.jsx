428826d9ecd9b3edb3159df1c882a64d2146dd92
21052103 <kiet2142004@gmail.com>
2024-12-18 23:40:10 +0700

    narbar

--- src/views/admin/HomeView.jsx
+++ src/views/admin/HomeView.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import Sidebar from "../../components/admin/SideBar";
+import Navbar from "../../components/admin/Navbar";
+
+
 
 const HomeView = () => {
   return (
-    <div className="home-container" style={{ display: "flex" }}>
-      {/* Sidebar */}
-      <Sidebar />
+    <div className="home-container" style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      
+      {/* Navbar */}
+      <Navbar />
 
-      {/* Nội dung chính */}
-      <div className="container text-center content" style={{ flexGrow: 1, padding: "20px" }}>
-        <h1>Welcome to Home Page</h1>
-        <p>This is the main content of the home view.</p>
+      <div style={{ display: "flex", flexGrow: 1 }}>
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Nội dung chính */}
+        <div className="container text-center content" style={{ flexGrow: 1, padding: "20px" }}>
+          <h1>Welcome to Home Page</h1>
+          <p>This is the main content of the home view.</p>
+        </div>
       </div>
     </div>
   );
